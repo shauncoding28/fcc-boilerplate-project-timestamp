c25ce5b316678813db4f9e83ca6ae173814ef93d
shauncoding28 <196330731+shauncoding28@users.noreply.github.com.>
2025-03-11 15:45:52 +0100

    add unix date get request

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +21,20 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
+//returns unix timestamp
+app.get("/api/:date", (req, res) => {
+  const date = req.params.date;
+  const unixDate = new Date(date).getTime();
+
+  if (isNaN(unixDate)) {
+    res.json({ error: "Date not valid" });
+  }
+  res.status(200).json({ unix: unixDate });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
